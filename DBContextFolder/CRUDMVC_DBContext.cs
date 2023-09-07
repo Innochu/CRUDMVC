de3b4764aaff5946c1dbef8274f81424dc3cea95
Chukwudi Innocent <chuksinnocent1@gmail.com>
2023-09-07 16:57:34 +0100

    my first commit/push for my second MVC project, all settings and connections strings created and set, also created tables and made migrations //we are doing a post request througth the form

---
+++ DBContextFolder/CRUDMVC_DBContext.cs
@@ -0,0 +1,17 @@
+﻿using CRUDMVC.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CRUDMVC.DBContextFolder
+
+{
+	public class CRUDMVC_DBContext : DbContext
+	{
+		public CRUDMVC_DBContext(DbContextOptions options) : base(options) // this constructor enables us pass the parameter to the base class
+		{
+
+		}
+        public DbSet<Employee>  Employees { get; set; }
+
+    }
+}
